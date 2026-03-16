ebfb505000ba3c52a78196bfacbb9408a987777f
이바울 <lbo119089@gmail.com>
2026-03-16 05:32:40 +0000

    api key 삽입

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,17 @@
-export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBEi2321Td9u-o7xSsL0IbNFlpEDgU3jo",
+  authDomain: "studio-4378421943-154fc.firebaseapp.com",
+  projectId: "studio-4378421943-154fc",
+  storageBucket: "studio-4378421943-154fc.firebasestorage.app",
+  messagingSenderId: "781814576985",
+  appId: "1:781814576985:web:0c15ec79acb12b99cabe48"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
