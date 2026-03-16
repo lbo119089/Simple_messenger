771a3945b201c7fc54616ad56823328b8a20b143
이바울 <lbo119089@gmail.com>
2026-03-16 05:33:22 +0000

    변경사항 적용

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBBEi2321Td9u-o7xSsL0IbNFlpEDgU3jo",
   authDomain: "studio-4378421943-154fc.firebaseapp.com",
@@ -12,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "781814576985",
   appId: "1:781814576985:web:0c15ec79acb12b99cabe48"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
