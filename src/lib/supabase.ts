8c18446dcdb8e525bde37d27deb46b2cbaa43b2b
이바울 <lbo119089@gmail.com>
2026-03-16 04:48:32 +0000

    supabase의 Database Direct connection string 은 'postgresql://postgres:[yw

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,14 +1,14 @@
+
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://vnszmzogkdqiicvpevlf.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_jFyIzw_S3Zizf-gJ8SLhCA_rN3v6lTI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export type Profile = {
   id: string;
   username: string;
-  full_name: string;
   avatar_url: string;
 };
 
